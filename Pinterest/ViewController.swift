8e1656f377b5735a8abee548e2ce6d6bb84f520c
Tania Ozuna <taniaozuna@MacBook-Pro-de-Tania.local>
2019-02-22 20:45:20 -0600

    first commit

--- Pinterest/ViewController.swift
+++ Pinterest/ViewController.swift
@@ -7,44 +7,55 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
       
 
     override func viewDidLoad() {
-        
-        view.addSubview(firstButton)
-        view.addSubview(profileImageView)
         super.viewDidLoad()
+        view.addSubview(firstButton)
+        //view.addSubview(profileImageView)
+        
         //view.addSubview(nameTextField)
-        //view.addSubview(passwordTextField)
+        
         
         view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
         view.addSubview(inputContainerView)
         inputContainerView.addSubview(nameTextField)
-        //inputContainerView.addSubview(passwordTextField)
+        inputContainerView.addSubview(passwordTextField)
+        inputContainerView.addSubview(emailTextField)
+        //inputContainerView.addSubview()
         
-        
-        
-        firstButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor).isActive = true
+        firstButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor, constant: 20).isActive = true
         firstButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         firstButton.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
+        firstButton.rightAnchor.constraint(equalTo: inputContainerView.rightAnchor).isActive = true
         
         inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
         inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+        //inputsContainerViewHeightAnchor?.isActive = true
         
             
         nameTextField.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor, constant: 12).isActive = true
         nameTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
         nameTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
-        nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier:1/3).isActive = true
+        //nameTextFieldHeightAnchor?.isActive = true
         
         passwordTextField.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor, constant: 12).isActive = true
-        passwordTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
         passwordTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
-        passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        //passwordTextFieldHeightAnchor?.isActive = true
+        
+        emailTextField.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor, constant: 12).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        emailTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        emailTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+
         // Do any additional setup after loading the view, typically from a nib.
         
     }
@@ -55,7 +66,6 @@ class ViewController: UIViewController {
         tf.backgroundColor = .white
         return tf
     }()
-}
 
 let passwordTextField : UITextField = {
     let tf = UITextField()
@@ -66,6 +76,14 @@ let passwordTextField : UITextField = {
     return tf
 }()
 
+let emailTextField : UITextField = {
+    let tf = UITextField()
+    tf.translatesAutoresizingMaskIntoConstraints = false
+    tf.placeholder = "email"
+    tf.keyboardType = .emailAddress
+    tf.backgroundColor = .white
+    return tf
+}()
 
 let inputContainerView : UIView = {
     let view = UIView()
@@ -77,28 +95,55 @@ let inputContainerView : UIView = {
     return view
 }()
 
-let firstButton : UIButton = {
+    lazy var firstButton : UIButton = {
     let ub = UIButton()
-    ub.backgroundColor = UIColor(red: 80/125, green: 101/255, blue: 161/255, alpha: 1)
-    ub.setTitle("Register",for: .normal)
+    ub.backgroundColor = UIColor(red: 88/255, green: 101/255, blue: 161/255, alpha: 1)
+    ub.setTitle("Register", for: .normal)
     ub.translatesAutoresizingMaskIntoConstraints = false
     ub.setTitleColor(UIColor.white, for: .normal)
-    //button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+    ub.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+    ub.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
     return ub
+  
 }()
 
-    var profileImageView: UIImageView = {
-    let imageView = UIImageView()
-    imageView.image = UIImage(named: "profileImage")
-    imageView.contentMode = .scaleAspectFill
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    //imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView)))
-    imageView.isUserInteractionEnabled = true
-    return imageView
-}()
-
-func setupProfileImageView(){
-    profileImageView.centerXAnchor.constraint(equalTo: inputContainerView.centerXAnchor).isActive = true
-    profileImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-    profileImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+@objc func handleButton(){
+    guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
+        print("Not valid")
+        return
+    }
+    Auth.auth().createUser(withEmail: email, password: password) { (auth:AuthDataResult?, error) in
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        var user = auth?.user
+        guard let uid = user?.uid else {
+            return
+        }
+        
+        //sucessfully
+        var ref = Database.database().reference(fromURL: "https://pinterest-1c540.firebaseio.com/")
+        let values = ["name" :name, "email": email]
+        let usersRef = ref.child("users").child(uid)
+        usersRef.updateChildValues(values, withCompletionBlock: { (error, databaseRef:DatabaseReference?) in
+            if  error != nil {
+                print(error)
+            }
+        })
+        
+        // successfully included
+        print("Saved user successfully into our database")
+        
+    }
 }
+
+}
+
+
+
+
+
+
